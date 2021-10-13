47f7fb8f84c74d0b3e26fd48c31f0869358accf3
MohonaJS <89448559+MohonaJS@users.noreply.github.com>
2021-10-13 21:16:05 +0200

    letter

--- OBJECT/letterFrequency/main.js
+++ OBJECT/letterFrequency/main.js
@@ -1,9 +1,14 @@
 function letterfreq(letter){
     let result = {};
-    for(let char of letter){
-        if(){
+    for(let i=0; i <letter.length;i++){
+
+        if(letter[i]){
+           result[letter[i]]=result[letter[i]]+1
             
+        }else{
+            result[letter[i]]=1
         }
 
-    }
+    } 
 }
+console.log(letterfreq("kallee")) 
